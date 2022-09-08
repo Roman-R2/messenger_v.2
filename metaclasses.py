bfec6b5ff05a71ff1ce18740184c26d59ec60b46
Roman <slyusarrv@yandex.ru>
2022-09-08 09:55:38 +0500

    complete task 02

--- metaclasses.py
+++ metaclasses.py
@@ -6,12 +6,11 @@ https://docs.python.org/3/library/dis.html
 
 import dis
 
-
 # Метакласс для проверки соответствия сервера:
 from pprint import pprint
 
 
-class ServerMaker(type):
+class ServerVerifier(type):
     def __init__(cls, clsname, bases, clsdict):
         # clsname - экземпляр метакласса - Server
         # bases - кортеж базовых классов - ()
@@ -26,12 +25,12 @@ class ServerMaker(type):
         # 'process_client_message': <function Server.process_client_message at 0x000000DACCE3E598>}
 
         # Список методов, которые используются в функциях класса:
-        methods = []    # получаем с помощью 'LOAD_GLOBAL'
+        methods = []  # получаем с помощью 'LOAD_GLOBAL'
         # Обычно методы, обёрнутые декораторами попадают
         # не в 'LOAD_GLOBAL', а в 'LOAD_METHOD'
         methods_2 = []  # получаем с помощью 'LOAD_METHOD'
         # Атрибуты, используемые в функциях классов
-        attrs = []      # получаем с помощью 'LOAD_ATTR'
+        attrs = []  # получаем с помощью 'LOAD_ATTR'
         # перебираем ключи
         for func in clsdict:
             # Пробуем
@@ -49,7 +48,7 @@ class ServerMaker(type):
             else:
                 # Раз функция разбираем код, получая используемые методы и атрибуты.
                 for i in ret:
-                    print(i)
+                    # print(i)
                     # i - Instruction(opname='LOAD_GLOBAL', opcode=116, arg=9, argval='send_message',
                     # argrepr='send_message', offset=308, starts_line=201, is_jump_target=False)
                     # opname - имя для операции
@@ -65,16 +64,17 @@ class ServerMaker(type):
                         if i.argval not in attrs:
                             # заполняем список атрибутами, использующимися в функциях класса
                             attrs.append(i.argval)
-        print(20*'-', 'methods', 20*'-')
-        pprint(methods)
-        print(20*'-', 'methods_2', 20*'-')
-        pprint(methods_2)
-        print(20*'-', 'attrs', 20*'-')
-        pprint(attrs)
-        print(50*'-')
+        # print(20*'-', 'methods', 20*'-')
+        # pprint(methods)
+        # print(20*'-', 'methods_2', 20*'-')
+        # pprint(methods_2)
+        # print(20*'-', 'attrs', 20*'-')
+        # pprint(attrs)
+        # print(50*'-')
         # Если обнаружено использование недопустимого метода connect, вызываем исключение:
         if 'connect' in methods:
-            raise TypeError('Использование метода connect недопустимо в серверном классе')
+            raise TypeError(
+                'Использование метода connect недопустимо в серверном классе')
         # Если сокет не инициализировался константами SOCK_STREAM(TCP) AF_INET(IPv4), тоже исключение.
         if not ('SOCK_STREAM' in attrs and 'AF_INET' in attrs):
             raise TypeError('Некорректная инициализация сокета.')
@@ -83,11 +83,17 @@ class ServerMaker(type):
 
 
 # Метакласс для проверки корректности клиентов:
-class ClientMaker(type):
+class ClientVerifier(type):
     def __init__(cls, clsname, bases, clsdict):
         # Список методов, которые используются в функциях класса:
         methods = []
+        # Атрибуты, используемые в функциях классов
+        attrs = []  # получаем с помощью 'LOAD_ATTR'
+        # Сохраненные наименования свойств класса
+        store_attrs = []
         for func in clsdict:
+            # print('clsdict'.center(69, '-'))
+            # pprint(clsdict)
             # Пробуем
             try:
                 ret = dis.get_instructions(clsdict[func])
@@ -97,16 +103,33 @@ class ClientMaker(type):
             else:
                 # Раз функция разбираем код, получая используемые методы.
                 for i in ret:
+                    # print(i)
                     if i.opname == 'LOAD_GLOBAL':
                         if i.argval not in methods:
                             methods.append(i.argval)
+                    elif i.opname == 'LOAD_ATTR':
+                        if i.argval not in attrs:
+                            # заполняем список атрибутами, использующимися в функциях класса
+                            attrs.append(i.argval)
+                    elif i.opname == 'STORE_ATTR':
+                        if i.argval not in attrs:
+                            # заполняем список сохраненных атрибутов класса
+                            store_attrs.append(i.argval)
         # Если обнаружено использование недопустимого метода accept, listen, socket бросаем исключение:
         for command in ('accept', 'listen', 'socket'):
             if command in methods:
-                raise TypeError('В классе обнаружено использование запрещённого метода')
+                raise TypeError(
+                    'В классе обнаружено использование запрещённого метода')
         # Вызов get_message или send_message из utils считаем корректным использованием сокетов
         if 'get_message' in methods or 'send_message' in methods:
             pass
         else:
-            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+            raise TypeError(
+                'Отсутствуют вызовы функций, работающих с сокетами.')
+            # Если сокет не инициализировался константами SOCK_STREAM(TCP) AF_INET(IPv4), тоже исключение.
+        if not (
+                'is_check_sock_stream' in store_attrs
+                and 'is_check_if_inet' in store_attrs
+        ):
+            raise TypeError('Некорректная инициализация сокета.')
         super().__init__(clsname, bases, clsdict)
